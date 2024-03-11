3ee54b1d929ee9d05b218496b04be473c82e1c89
root <root@DESKTOP-BG009UJ>
2024-03-11 23:14:41 +0300

    add currency  validator

--- api/server.go
+++ api/server.go
@@ -2,24 +2,35 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/weldonkipchirchir/simple_bank/db/sqlc"
 )
 
 // server serves HTTP requests for the banking service
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and sets up routing
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	//register validator
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	//transfer
+	router.POST("/transfer", server.createTransfer)
 
 	server.router = router
 	return server
