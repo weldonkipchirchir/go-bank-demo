46f42af055ba7533c0d33b75df52c0876d42473c
root <root@DESKTOP-BG009UJ>
2024-03-11 23:16:56 +0300

    add transfer api

---
+++ api/transfer.go
@@ -0,0 +1,182 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/weldonkipchirchir/simple_bank/db/sqlc"
+)
+
+type transferRequest struct {
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+
+	}
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, result)
+
+}
+
+// compare currency of to and from account . It checks if an account with certain id exits
+func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s ", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+
+}
+
+// type getAccountRequest struct {
+// 	ID int64 `uri:"id" binding:"required,min=1"`
+// }
+
+// func (server *Server) getAccount(ctx *gin.Context) {
+// 	var req getAccountRequest
+// 	if err := ctx.ShouldBindUri(&req); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+// 	account, err := server.store.GetAccount(ctx, req.ID)
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
+// 			return
+// 		}
+// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, account)
+
+// }
+
+// type listAccountRequest struct {
+// 	PageID   int32 `form:"page_id" binding:"required,min=1"`
+// 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+// }
+
+// func (server *Server) listAccount(ctx *gin.Context) {
+// 	var req listAccountRequest
+// 	if err := ctx.ShouldBindQuery(&req); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+
+// 	arg := db.ListAccountsParams{
+// 		Limit:  req.PageSize,
+// 		Offset: (req.PageID - 1) * req.PageSize, //no of record db should skip
+// 	}
+// 	accounts, err := server.store.ListAccounts(ctx, arg)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, accounts)
+
+// }
+
+// type updateAccountRequest struct {
+// 	ID int64 `uri:"id" binding:"required,min=1"`
+// }
+
+// type updateAccountRequest2 struct {
+// 	Balance int64 `json:"balance" binding:"required"`
+// }
+
+// func (server *Server) updateAccount(ctx *gin.Context) {
+// 	var req updateAccountRequest
+// 	if err := ctx.ShouldBindUri(&req); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+
+// 	var req2 updateAccountRequest2
+// 	if err := ctx.ShouldBindJSON(&req2); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+
+// 	arg := db.UpdateAccountParams{
+// 		ID:      req.ID,
+// 		Balance: req2.Balance,
+// 	}
+// 	account, err := server.store.UpdateAccount(ctx, arg)
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
+// 			return
+// 		}
+// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, account)
+
+// }
+
+// type deleteAccountRequest struct {
+// 	ID int64 `uri:"id" binding:"required,min=1"`
+// }
+
+// func (server *Server) deleteAccount(ctx *gin.Context) {
+// 	var req deleteAccountRequest
+// 	if err := ctx.ShouldBindUri(&req); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+// 	err := server.store.DeleteAccount(ctx, req.ID)
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
+// 			return
+// 		}
+// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, gin.H{"Message": "Successfully deleted"})
+
+// }
