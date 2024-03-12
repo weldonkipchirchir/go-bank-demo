82686e0bcb8f795b5789b9b0943abe1d981558a2
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-03-12 16:36:52 +0300

    changed Owner value

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -12,8 +12,10 @@ import (
 
 // makes test are independent
 func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
